b5561007b42688986e9f0e1ae87515d0a6680e69
Asli Nur Karaman <karamanasli7868@gmail.com>
2022-07-04 15:56:44 +0300

    Add project files.

---
+++ ZarAtma/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZarAtma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int a=random.Next(1,7);
+            int b=random.Next(1,7);
+            label1.Text=a.ToString();
+            label2.Text=b.ToString();
+        }
+    }
+}
